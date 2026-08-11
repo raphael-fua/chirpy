34a633baaa307b8be48d62ee36c949655139c744
Raphael Fua <raphael.fua@epfl.ch>
2026-08-11 09:55:21 +0200

    3.1 - Monoliths and Decoupling

--- main.go
+++ main.go
@@ -1,14 +1,58 @@
 package main
 
 import (
+	"fmt"
 	"log"
 	"net/http"
+	"sync/atomic"
 )
 
+type apiConfig struct {
+	fileserverHits atomic.Int32
+}
+
+
+
 func main() {
+	const filepathRoot = "."
 	const port = "8080"
 
 	mux := http.NewServeMux()
+
+	apiCfg := apiConfig{
+		fileserverHits: atomic.Int32{},
+	}
+
+	mux.Handle("/app/", apiCfg.middlewareMetricsInc(
+		http.StripPrefix("/app", http.FileServer(http.Dir(filepathRoot)))))
+
+		
+		
+
+	mux.HandleFunc(
+		"GET /api/healthz",
+		func(w http.ResponseWriter, r *http.Request){
+ 			w.Header().Add("Content-Type", "text/plain; charset=utf-8")
+			w.WriteHeader(http.StatusOK)
+			w.Write([]byte(http.StatusText(http.StatusOK)))
+		})
+
+	mux.HandleFunc(
+		"GET /api/metrics", 
+		func(w http.ResponseWriter, r *http.Request){
+ 			w.Header().Add("Content-Type", "text/plain; charset=utf-8")
+			w.WriteHeader(http.StatusOK)
+			w.Write([]byte(fmt.Sprintf("Hits: %d\n", apiCfg.fileserverHits.Load())))
+		})
+
+	mux.HandleFunc(
+		"POST /api/reset", 
+		func(w http.ResponseWriter, r *http.Request){
+			apiCfg.fileserverHits.Store(0)
+			w.WriteHeader(http.StatusOK)
+			w.Write([]byte("Hits reset to 0\n"))
+		})
+
 
 	srv := &http.Server{
 		Addr:    ":" + port,
@@ -18,3 +62,21 @@ func main() {
 	log.Printf("Serving on port: %s\n", port)
 	log.Fatal(srv.ListenAndServe())
 }
+
+
+func (cfg *apiConfig) middlewareMetricsInc(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request){
+		cfg.fileserverHits.Add(1)
+		next.ServeHTTP(w, r)
+	})
+}
+
+
+
+
+
+
+
+
+
+
